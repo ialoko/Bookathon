26bd9c74cf4018a1e6616bcd2fc28d484edb9b6b
Iyiola <48067557+ialoko@users.noreply.github.com>
2022-08-26 00:28:58 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
 
 //middleware
 app.use(express.static('public'));
 
+// Connect to the database
+mongoose
+    .connect(process.env.DB, { useNewUrlParser: true })
+    .then(() => console.log(`Database connected successfully`))
+    .catch((err) => console.log(err));
 
+// Since mongoose's Promise is deprecated, we override it with Node's Promise
+mongoose.Promise = global.Promise;
 
+app.use(bodyParser.json());
 
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    next();
+});
 
 
 app.listen(port, () => {
