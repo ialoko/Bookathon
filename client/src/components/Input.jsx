1c695fb44c1a10331f76b132771e855de09fb627
Iyiola <48067557+ialoko@users.noreply.github.com>
2023-02-18 21:31:28 -0500

    updated Input.jsx

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+const cheerio = require('cheerio'); 
 //https://www.gutenberg.org/browse/scores/top#books-last30
 //https://www.gutenberg.org/ebooks/64317
 //https://www.gutenberg.org/cache/epub/64317/pg64317.cover.medium.jpg
@@ -45,10 +46,47 @@ class Input extends Component {
             console.log('input field required');
         }
     };
+//th - Author, td author name
+    //grab ebook info from project gutenberg based on ebook number
+    getBookInfo = () => {
+        if(this.state.ebookNo > 0){ //user entered ebook number
+            const author = "";
+            const title = ""
+            const subject = ""
+            axios.get(`https://www.gutenberg.org/ebooks/${this.state.ebookNo}`).then(response => {
+                if(response.status === 200){
+                    const html = response.data;
+                    const $ = cheerio.load(html);
+                    $("#bibrec > div > table > tbody > tr").each((index, element) => {
+                        if($(element).text().equals("Author")){
+                            author = $(element).next.text()
+                        }
+                        else if($(element).text().equals("Title")){
+                            title = $(element).next.text()
+                        }
+                        else if($(element).text().equals("Subject") && subject.equals("")){
+                            subject = $(element).next.text()
+                        }
+                    });
+                }
+            }).catch(err => console.error(err))
+        }
+        this.addBookInfo(author,title,subject);
+    };
 
     handleChange = (e) => {
         this.setState({
-            action: e.target.value,
+            ebookNo: e.target.value,
+        });
+    };
+
+    addBookInfo = (author, title, subject){
+        imgString = `https://www.gutenberg.org/cache/epub/${this.state.ebookNo}/pg${this.state.ebookNo}.cover.medium.jpg`
+        this.setState({
+            title: title,
+            author: author,
+            description: subject, 
+            img: imgString,
         });
     };
 
