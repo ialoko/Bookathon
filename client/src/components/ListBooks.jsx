0ce173cc6f8761032f2fce33d9b12745bde9c7cd
Iyiola <48067557+ialoko@users.noreply.github.com>
2023-02-19 21:05:55 -0500

    fixed checkbox alignment

--- client/src/components/ListBooks.jsx
+++ client/src/components/ListBooks.jsx
@@ -6,14 +6,14 @@ const ListBooks = ({ books, deleteBook, handleFinished }) => {
       {books && books.length > 0 ? (
         books.map((book) => {
           return (
-            <div>
-                <li key={book._id}>
+            <div key={book._id}>
+                <li>
                 <p> Title : {book.title} <br/>  Author : {book.author} <br/> </p>
                 <img src= {book.img} alt="Book cover"></img>
                 <br/>
-                <p>Description : {book.description} <br/></p>
-                <label><input type="checkbox" checked={book.finished} onChange={() => handleFinished(book._id, !book.finished)} /> Completed</label>
-                <br/><button onClick={() => deleteBook(book._id)} > Delete Book</button> 
+                <p>Description : {book.description}</p>
+                <p class="container"> Complete <input type="checkbox" checked={book.finished} onChange={() => handleFinished(book._id, !book.finished)}/> <span class="checkmark"></span></p>
+                <button onClick={() => deleteBook(book._id)} > Delete Book</button> 
                 </li>
             </div>   
           );
